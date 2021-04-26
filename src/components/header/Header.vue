c84ad5a24a2217d7edc87ac0f59fec46bb867aed
Zhao <cho.chubun@fujitsu-general.com>
2021-04-26 10:56:55 +0900

    [Update] Init commit
    1. Add header
    2. Add tab

---
+++ src/components/header/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <span class="icon"></span>
+    <h1 class="text">Movie Finder</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Header'
+})
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 44px;
+  text-align: center;
+  color: $color-theme;
+  font-size: 0;
+
+  .icon {
+    display: inline-block;
+    vertical-align: top;
+    width: 30px;
+    height: 32px;
+    margin-right: 9px;
+    @include bg-image('logo.png');
+    background-size: 30px 32px;
+  }
+
+  .text {
+    display: inline-block;
+    vertical-align: top;
+    line-height: 44px;
+    font-size: $font-size-large;
+  }
+}
+</style>
---
+++ src/components/tab/Tab.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tab">
+    <router-link
+      class="tab-item"
+      v-fot="tab in tabList"
+      :key="tab.path"
+      :to="tab.path"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Tab',
+  data () {
+    return {
+      tabList: [
+        {
+          name: 'Search',
+          path: '/search'
+        },
+        {
+          name: 'Recommend',
+          path: '/recommend'
+        }
+      ]
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.tab {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  font-size: $font-size-medium;
+
+  .tab-item {
+    flex: 1;
+    text-align: center;
+
+    .tab-link {
+      padding-bottom: 5px;
+      color: $color-text-l;
+    }
+
+    &.router-link-active {
+      .tab-link {
+        color: $color-text-theme;
+        border-bottom: 2px solid $color-theme;
+      }
+    }
+  }
+
+}
+
+</style>
